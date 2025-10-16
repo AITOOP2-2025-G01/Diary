bb708679fc0ce1d1ac3e9345bb4721a9d92564da
告白 <kout5504@gmail.com>
2025-10-16 15:16:18 +0900

    add my diary

---  main.py
+++  main.py
@@ -1,7 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.ZhengHaotianDiary import ZhengHaotianDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), 
+            ZhengHaotianDiary(),]
 
 for d in diaries:
     print("---------------------------------")
